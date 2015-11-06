a4badf4b98d5dcc8e4795a7e4ab3fd5278728ab3
Federico Fiori <federico.fiori@axant.it>
2015-11-06 15:57:29 +0100

    Raised version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ except IOError:
 
 setup(
     name='tgapp-fbauth',
-    version='0.1.1',
+    version='0.2.0',
     description='Facebook Authentication for TurboGears2',
     long_description=README,
     author='AXANT',
